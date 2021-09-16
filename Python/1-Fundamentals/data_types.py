1dc2b70b7424d571d32995973a033e4b556a2423
Eric Kim <kim.eric.94@gmail.com>
2021-09-16 10:41:58 -0400

    repo init

---
+++ Python/1-Fundamentals/data_types.py
@@ -0,0 +1,26 @@
+name = "Eric"
+age = 27
+cash = 420.69
+sexy = True
+
+# print("The data type for variable 'name' is:", type(name))
+# print("The data type for variable 'age' is:", type(age))
+# print("The data type for variable 'cash' is:", type(cash))
+# print("The data type for variable 'sexy' is:", type(sexy))
+
+# Data composed as a `List`; ordered sequence of multiple values
+nucamp_locations = {"Seattled", "Tacoma", "Bellevue"}
+
+# Data composed as a `Dictionary`; unordered collection of key-value pairs; equivalent to an array
+Eric_Info = {"name": "Eric", "age": 27, "cash": 420.69, "sexy": True}
+
+# Data composed as a `Tuple`; similiar to a list, but immutable
+my_cool_tuple = {"apple", "bannana", "cherry"}
+
+# Data composed as a `Set`; unordered collection of values, immutable, no duplicate values
+my_cool_set = {"cats", "dogs", "bridges"}
+
+print("The data type for variable 'nucamp_locations' is:", type(nucamp_locations))
+print("The data type for variable 'Eric_Info' is:", type(Eric_Info))
+print("The data type for variable 'my_cool_tuple' is:", type(my_cool_tuple))
+print("The data type for variable 'my_cool_set' is:", type(my_cool_set))
