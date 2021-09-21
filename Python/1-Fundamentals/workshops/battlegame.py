3c7ff61b3f6061ea98d6caa2498c246b4a6dc6ac
Eric Kim <kim.eric.94@gmail.com>
2021-09-21 13:12:42 -0400

    exit option

--- Python/1-Fundamentals/workshops/battlegame.py
+++ Python/1-Fundamentals/workshops/battlegame.py
@@ -8,46 +8,54 @@ wizard_hp = 70
 elf_hp = 100
 human_hp = 150
 orc_hp = 9000
+exit_hp = 0
 
 wizard_dmg = 150
 elf_dmg = 100
 human_dmg = 20
 orc_dmg = 175
+exit_dmg = 0
 
 dragon_hp = 300
 dragon_dmg = 50
 
 while True:
-    # char_choice is class `str`
-    char_choice = input(
-        "1) Wizard\n2) Elf\n3) Human\n4) Orc\n" "Choose your character: ").lower()
-    if char_choice == 'wizard' or char_choice == '1':
+    # option is class `str`
+    option = input(
+        "1) Wizard\n2) Elf\n3) Human\n4) Orc\n5) Exit\n" "Choose your character: ").lower()
+    if option == 'wizard' or option == '1':
         character = wizard
         my_hp = wizard_hp
         my_dmg = wizard_dmg
         print("You have chosen the character: ", character, "\nHealth: ",
               my_hp, "\nDamage: ", my_dmg)
         break
-    if char_choice == 'elf' or char_choice == '2':
+    if option == 'elf' or option == '2':
         character = elf
         my_hp = elf_hp
         my_dmg = elf_dmg
         print("You have chosen the character: ", character, "\nHealth: ",
               my_hp, "\nDamage: ", my_dmg)
         break
-    if char_choice == 'human' or char_choice == '3':
+    if option == 'human' or option == '3':
         character = human
         my_hp = human_hp
         my_dmg = human_dmg
         print("You have chosen the character: ", character, "\nHealth: ",
               my_hp, "\nDamage: ", my_dmg)
         break
-    if char_choice == 'orc' or char_choice == '4':
+    if option == 'orc' or option == '4':
         character = orc
         my_hp = orc_hp
         my_dmg = orc_dmg
         print("You have chosen the character: ", character, "\nHealth: ",
               my_hp, "\nDamage: ", my_dmg)
+        break
+    if option == 'exit' or option == '5':
+        character = exit
+        my_hp = exit_hp
+        my_dmg = exit_dmg
+        print("You have fled the battle!")
         break
     else:
         print("Unknown character!")
@@ -55,6 +63,8 @@ while True:
 # Task 4 - Battle w/ the Dragon!
 
 while True:
+    if character == 'exit':
+        break
     dragon_hp = dragon_hp - my_dmg
     print("\nThe", character, "damaged the Dragon!")
     print("The Dragon's hitpoints are now:", dragon_hp, "\n")
