7eb03875ea12216ea20fbe6fdab4307e775d8b1c
Eric Kim <kim.eric.94@gmail.com>
2021-09-21 17:08:48 -0400

    finished: cc_staircase_loop w/ line tracing! WIP: intro to functions

--- Python/1-Fundamentals/coding_challenges/cc_staircase_loop.py
+++ Python/1-Fundamentals/coding_challenges/cc_staircase_loop.py
@@ -18,3 +18,42 @@ for i in range(0, 5, 1):
     for j in range(0, i, 1):
         stars += "*"
     print(stars)
+
+
+"""
+when i = 0, j range(0, 0)
+>> ""
+
+when i = 1, j range(0, 1)
+>> "" + * --> *
+
+when i = 2, j range(0, 2)
+>> * + ** --> ***
+
+when i = 3, j range(0, 3)
+>> *** + *** -- > ******
+
+when i = 4, j range(0, 4)
+>> ****** + **** ---> **********
+
+"""
+
+
+"""
+stars = ""
+for x in range(5):
+    stars += "*" * x
+    print(stars)
+
+when x = 0
+>> "" + ('*' * 0) = "" + 0 = ""
+
+when x = 1
+>> "" + ('x' * 1) = "" + * = *
+
+when x = 2
+>> * + ('x' * 2) = * + ** = ***
+
+etc...
+
+"""
--- Python/1-Fundamentals/notes/scratchpad.py
+++ Python/1-Fundamentals/notes/scratchpad.py
@@ -1,43 +1,18 @@
-# # x = 1 + (0 * 10) * 3 / 8 ** 1
-# # print(type(x))
+stars = ""
+for x in range(5):
+    stars += "*" * x
+    print(stars)
 
-# # y = 0 * 10
-# # print(type(y))
+"""
+when x = 0
+>> "" + ('*' * 0) = "" + 0 = ""
 
-# # z = 8 ** 1
-# # print(type(z))
+when x = 1
+>> "" + ('x' * 1) = "" + * = *
 
-# # g = 0 * 3
-# # print(type(g))
+when x = 2
+>> * + ('x' * 2) = * + ** = ***
 
-# f = 4 / 8
+etc...
 
-# # print("this is the float", type(f))
-
-# # h = 1 + 0
-# # print(type(h))
-
-# x = f * 2
-# print("is this a float?", type(x))
-# print(x)
-
-# """
-# PEMDAS
-
-# (1) 0 * 10 = 0
-# (2) 8 ** 1 = 8
-# (3) 0 * 3 = 0
-# (4) 0 / 8 = 0
-# (5) 1 + 0 = 1
-# x = 1
-
-
-# """
-# stars = "*"
-# for i in range(1, 5, 2):
-#     for j in range(0, 2, 1):
-#         print(i)
-# for j in range(0, 2, 1):
-#     print(j)
-for i in range(0, 5, 1):
-    print(i)
+"""
