c261fc0c5cf5c92518ec640758db34e6d9bf5e48
ericdwkim <kim.eric.94@gmail.com>
2021-09-24 14:32:16 -0400

    portfolio brainstorming

---
+++ Python/1-Fundamentals/portfolio_project/test.py
@@ -0,0 +1,15 @@
+player_role = ["attacker", "defender"]
+"""
+bba = 4 
+mook = 5
+jji = 3
+mook > jji
+
+jji > bba 
+"""
+
+
+def roleMaker(option):
+    option = ("mook", "jji", "bba")
+    return player_role
+    if player_role == "attacker":
