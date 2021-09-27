ebf1d544b024978d0ea05d782c998d81c4621b12
Eric Kim <kim.eric.94@gmail.com>
2021-09-27 13:54:31 -0400

    finished cc_cards; works as expected!

---
+++ Python/1-Fundamentals/coding_challenges/cc_cards.py
@@ -0,0 +1,20 @@
+import random
+
+diamonds = ["AD", "2D", "3D", "4D", "5D", "6D",
+            "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+hand = []
+
+while diamonds:
+    option = input("Press enter to pick a card, or Q + enter to quit\n")
+    # user hits enter keystroke
+    if option == "":
+        dia_random = random.choice(diamonds)
+        diamonds.remove(dia_random)
+        hand.append(dia_random)
+        print(hand[0], "has been randomly chosen and added to your hand\n")
+        print("Remaining cards", diamonds)
+        print("Your hand", hand)
+    if option == "Q":
+        break
+if not diamonds:
+    print("There are no more cards to pick.")
