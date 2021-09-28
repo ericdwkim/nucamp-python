ac62308dc15f5e7196c62fc638f9c0f779c99593
ericdwkim <kim.eric.94@gmail.com>
2021-09-28 18:53:14 -0400

    user input can be typecasted as an int

--- Python/1-Fundamentals/week3/cc/cc_dictionary.py
+++ Python/1-Fundamentals/week3/cc/cc_dictionary.py
@@ -10,17 +10,10 @@ def print_total_snowfall(inches_snow):
 
 print_total_snowfall(inches_snow)
 
-user_input = input(
-    "How many inches of snow fell on Thursday? ")
+
+inches_snow["Thursday"] = int(input(
+    "How many inches of snow fell on Thursday? "))
+
 print_total_snowfall(inches_snow)
 
-inches_snow["Thursday"] = user_input
-print(inches_snow)
-inches_snow["Thursday"]
-
-# print("did it work?", inches_snow)
-
-
-# print_total_snowfall(inches_snow)
-# inches_snow["Thursday"] = user_input
-# print("did it work?", inches_snow)
+# print(inches_snow)
