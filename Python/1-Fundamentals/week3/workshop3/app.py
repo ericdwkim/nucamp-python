aa254980d4e7e32842181fe9218b1f9826c8bd93
Eric Kim <kim.eric.94@gmail.com>
2021-10-04 12:07:40 -0400

    fixed: quit option; if/else inside while loop; ifs into elifs

--- Python/1-Fundamentals/week3/workshop3/app.py
+++ Python/1-Fundamentals/week3/workshop3/app.py
@@ -5,31 +5,36 @@ database = {"admin": "password123"}
 donations = []
 authorized_user = ""
 
-if authorized_user == "":
-    print("You must be logged in to donate.")
-else:
-    print("Logged in as:", authorized_user)
+# if authorized_user == "":
+#     print("You must be logged in to donate.")
+# else:
+#     print("Logged in as:", authorized_user)
 
 while True:
+    if authorized_user == "":
+        print("You must be logged in to donate.")
+    else:
+        print("Logged in as:", authorized_user)
     show_homepage()
     option = input("Choose an option: ")
     if option == "1":
         username = input("Enter username: ")
         password = input("Enter password: ")
         authorized_user = login(database, username, password)
-    if option == "2":
+    elif option == "2":
         username = input("Enter username: ")
         password = input("Enter password: ")
         authorized_user = register(database, username)
         if authorized_user != "":
             database[username] = password
-    if option == "3":
+    elif option == "3":
         if authorized_user == "":
             print("You are not logged in.")
         else:
             donation = donate(username)
             donations.append(donation)
-    if option == "4":
+    elif option == "4":
         show_donations(donations)
-    if option == "5":
+    elif option == "5":
         print("Leaving DonateMe....")
+        quit()
