e0d02aec040d37805b596730a12a0d36da18accc
Eric Kim <kim.eric.94@gmail.com>
2021-10-07 11:28:31 -0400

    WIP: code challenge prepend()

--- Python/1-Fundamentals/week4/ccs/cc_linked_list3.py
+++ Python/1-Fundamentals/week4/ccs/cc_linked_list3.py
@@ -23,8 +23,22 @@ class LinkedList:
         node.next = new_node
         print("Append new Node with value:", node.next.value)
 
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            print("Head Node created: ", self.head.value)
+            return
+
+        node = self.head
+        while node is not None:
+            node = new_node
+            print("Prepended new Head Node with value:", node.value)
+            node = node.next
+            print("Node following Head is:", node.value)
+
 
 llist = LinkedList()
-llist.append("First Node")
-llist.append("Second Node")
-llist.append("Third Node")
+llist.prepend("First Node")
+llist.prepend("Inserted New First Node")
