1f8349479ad5e6d3ce493d27f6301ae0bed0067a
ericdwkim <kim.eric.94@gmail.com>
2021-10-09 18:06:14 -0400

    finished: BankUser workshop #4; all 5 test tasks passing!

--- Python/1-Fundamentals/week4/workshop/workshop4.py
+++ Python/1-Fundamentals/week4/workshop/workshop4.py
@@ -28,11 +28,13 @@ class BankUser(User):
     def deposit(self, balance):
         self.balance += balance
 
-    def transfer_money(self, pin, tAmount, tUser):
+    def transfer_money(self, tAmount, tUser):
         print(f'\nYou are transferring ${tAmount} to {tUser.name}')
         print("Authentication required")
-        print(f'Enter your PIN: {self.pin}')
-        if pin == self.pin:
+        pin_to_validate = input("Enter your PIN: ")
+        if pin_to_validate != self.pin:
+            print("Invalid PIN. Transaction canceled.")
+        elif pin_to_validate == self.pin:
             print("Transfer authorized")
             print(f'Transferring ${tAmount} to {tUser.name}')
             self.balance -= int(tAmount)
@@ -41,17 +43,21 @@ class BankUser(User):
         else:
             return False
 
-    def request_money(self, pin, password, tAmount, tUser):
+    def request_money(self, tAmount, tUser):
         print(f'\nYou are requesting ${tAmount} from {tUser.name}')
         print("User authentication is required...")
-        print(f'Enter {tUser.name}\'s PIN: {tUser.pin}')
-        print(f'Enter your password: {self.password}')
-        if pin == self.pin:
-            if password == self.password:
+        pin_to_validate = input(f'Enter {tUser.name}\'s PIN: ')
+        pw_to_validate = input("Enter your password: ")
+        if pin_to_validate != tUser.pin:
+            print("Invalid PIN. Transaction canceled.")
+        elif pin_to_validate == tUser.pin:
+            if pw_to_validate == self.password:
                 print("Request authorized")
                 print(f'{tUser.name} sent ${tAmount}')
                 tUser.balance -= int(tAmount)
                 self.balance += int(tAmount)
+            elif pw_to_validate != self.password:
+                print("Invalid password. Transaction canceled.")
             else:
                 return False
         else:
@@ -65,6 +71,7 @@ class BankUser(User):
 # print(user1.name, user1.pin, user1.password)
 
 """ Driver Code for Task 2 """
+# user1 = User("Bob", "1234", "password")
 
 # user1.change_name("pancake")
 # user1.change_pin("4321")
@@ -80,7 +87,7 @@ class BankUser(User):
 
 """ Driver Code for Task 4 """
 
-# bankUser1 = BankUser("Joe", "1111", "coolPass")
+# bankUser1 = BankUser("Joe", "4747", "coolPass")
 
 # bankUser1.show_balance()
 
@@ -92,29 +99,26 @@ class BankUser(User):
 
 """ Driver Code for Task 5a - transfer_money() """
 
-# bankUser1 = BankUser("Joe", "1111", "coolPass")
-# bankUser2 = BankUser("Sally", "1000", "whatever")
+# bankUser1 = BankUser("Joe", "4747", "coolPass")
+# bankUser2 = BankUser("Sally", "0101", "whatever")
 # bankUser2.show_balance()
 
 # bankUser1.deposit(50)
 # bankUser1.show_balance()
 
-# bankUser1.transfer_money("1111", "10", bankUser2)
+# bankUser1.transfer_money(10, bankUser2)
 # bankUser1.show_balance()
+# bankUser2.show_balance()
 
 """ Driver Code for Task 5b - request_money() """
 
-bankUser1 = BankUser("Joe", "1111", "coolPass")
-bankUser2 = BankUser("Sally", "1000", "whatever")
+# bankUser1 = BankUser("Joe", "4747", "coolPass")
+# bankUser2 = BankUser("Sally", "0101", "whatever")
 
-bankUser2.deposit(5000)
-bankUser2.show_balance()
-bankUser1.show_balance()
+# bankUser2.deposit(5000)
+# bankUser2.show_balance()
+# bankUser1.show_balance()
 
-bankUser2.transfer_money("1000", 500, bankUser1)
-bankUser2.show_balance()
-bankUser1.show_balance()
-
-bankUser2.request_money("1000", "whatever", 250, bankUser1)
-bankUser2.show_balance()
-bankUser1.show_balance()
+# bankUser2.request_money(250, bankUser1)
+# bankUser2.show_balance()
+# bankUser1.show_balance()
