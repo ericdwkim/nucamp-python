762bb50b5c8eebeabf9deccbd000d4ff5ef696e1
Eric Kim <kim.eric.94@gmail.com>
2021-11-10 14:38:19 -0500

    finished: Flask exercise part 2 w/ notes

--- Python/2-SQL/notes/week3/models.notes.py
+++ Python/2-SQL/notes/week3/models.notes.py
@@ -1,6 +1,7 @@
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 
+# creating a db adapter using SQLAlchemy
 db = SQLAlchemy()
 
 # Reference:
@@ -9,12 +10,18 @@ db = SQLAlchemy()
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/#many-to-many-relationships
 
 
+# creates User as a subclass of SQLAlchemy's db.Model class
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+    # back reference (part 1)
     tweets = db.relationship('Tweet', backref='user', cascade="all,delete")
+
+# creates Like model; a many-to-many relationship w/ User and Tweet
 
 
 likes_table = db.Table(
@@ -39,6 +46,8 @@ likes_table = db.Table(
 )
 
 
+# creates Tweet model
+
 class Tweet(db.Model):
     __tablename__ = 'tweets'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -48,7 +57,12 @@ class Tweet(db.Model):
         default=datetime.datetime.utcnow,
         nullable=False
     )
+
+    # equivalent to adding the fk column; creates the relationship b/w User and Tweet models
+    # and thus the tweets table and the users table
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # back reference (part 2)
     likes = db.relationship(
         'User', secondary=likes_table,
         lazy='subquery',
