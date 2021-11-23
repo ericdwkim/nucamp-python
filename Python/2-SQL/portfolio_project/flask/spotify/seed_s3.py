a8f99e6463d45f0c15076dd3efec22f0f81763ab
Eric Kim <kim.eric.94@gmail.com>
2021-11-23 13:50:10 -0500

    (added): dunder name

--- Python/2-SQL/portfolio_project/flask/spotify/seed_s3.py
+++ Python/2-SQL/portfolio_project/flask/spotify/seed_s3.py
@@ -16,39 +16,44 @@ access_key = os.getenv('access_key')
 access_secret = os.getenv('access_secret')
 bucket_name = os.getenv('bucket_name')
 
-"""
-Connect to S3 Service
-"""
-client_s3 = boto3.client(
-    's3',
-    aws_access_key_id=access_key,
-    aws_secret_access_key=access_secret
-)
 
-print(client_s3)
+def main():
+    """
+    Connect to S3 Service
+    """
+    client_s3 = boto3.client(
+        's3',
+        aws_access_key_id=access_key,
+        aws_secret_access_key=access_secret
+    )
+
+    print(client_s3)
+
+    """
+    Upload Files to S3 Bucket
+
+    """
+    data_file_path = os.path.join(os.getcwd(), 'audio_tracks')
+    for file in os.listdir(data_file_path):
+        if not file.startswith('~'):
+            try:
+                print('Uploading file {0}...'.format(file))
+                client_s3.upload_file(
+                    os.path.join(data_file_path, file),
+                    bucket_name,
+                    file
+                )
+            except ClientError as e:
+                print('Credential is incorrect')
+                print(e)
+            except Exception as e:
+                print(e)
+
+    """
+    Download Files to S3 Bucket
+
+    """
 
 
-"""
-Upload Files to S3 Bucket
-
-"""
-data_file_path = os.path.join(os.getcwd(), 'audio_tracks')
-for file in os.listdir(data_file_path):
-    if not file.startswith('~'):
-        try:
-            print('Uploading file {0}...'.format(file))
-            client_s3.upload_file(
-                os.path.join(data_file_path, file),
-                bucket_name,
-                file
-            )
-        except ClientError as e:
-            print('Credential is incorrect')
-            print(e)
-        except Exception as e:
-            print(e)
-
-"""
-Download Files to S3 Bucket
-
-"""
+if __name__ == '__main__':
+    main()
