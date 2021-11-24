f58f985024b56cee229b72a4279c156ef135bcd8
Eric Kim <kim.eric.94@gmail.com>
2021-11-24 13:08:25 -0500

    (cleaned): seed db script; (wip): enable permissions for each s3 object programmatically

--- Python/2-SQL/portfolio_project/flask/spotify/seed_db.py
+++ Python/2-SQL/portfolio_project/flask/spotify/seed_db.py
@@ -1,13 +1,8 @@
-"""
-TODO: Populate spotify database with personal data using the SQLAlchemy ORM.
-"""
+# NOTE: Populates spotify.audios db table with local audio files using the SQLAlchemy ORM.
+
 import os
 import boto3
 from dotenv import load_dotenv
-import random
-import string
-import hashlib
-import secrets
 import urllib
 from src.models import Account, Album, Artist, Group, Song, Audio, songs_artists, songs_albums, albums_artists, groups_artists, accounts_artists, db
 from src import create_app
@@ -20,10 +15,10 @@ s3_bucket = os.getenv('bucket_name')
 
 
 def main():
+
     # Main driver function
     app = create_app()
     app.app_context().push()
-    # truncate_tables()
 
     # Connect to S3 service
     s3 = boto3.client(
@@ -37,18 +32,10 @@ def main():
         return list_resp
 
     def store_urls(list_url):
-        # iterate through each url element in list_url
-        # store each url element into audios table, creating new audio records w/ respective urls
         for url in list_url:
             audio = Audio(url)
             db.session.add(audio)
         db.session.commit()
-
-        """
-        NOTE: print((k['Key'])) # returns audio file
-        NOTE: print(list_url_objs) # returns urls in list data structure
-
-        """
 
     prefix = "https://s3.us-east-2.amazonaws.com/my.audio.tracks/"
     for k in s3.list_objects(Bucket=s3_bucket)['Contents']:
@@ -56,8 +43,16 @@ def main():
             prefix + urllib.parse.quote(k['Key'], safe="~()*!.'"))
         store_urls(list_url_obj)
 
+
     # TODO: object url requires configuring permissions for each individual s3 object;
     # programmatically access each s3 object; see SAMPLE ACL:
+"""
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html#sample-acl
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/amazon-s3-policy-keys.html
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-arn-format.html
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/access-policy-language-overview.html
+
+"""
 
 
 if __name__ == '__main__':
