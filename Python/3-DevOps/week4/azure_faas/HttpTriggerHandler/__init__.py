31f365365be06f0a8d1f0650e94647b6144c742f
ericdwkim <ericdwkim.io@gmail.com>
2021-12-22 11:30:50 -0500

    (wip): azure_faas module

---
+++ Python/3-DevOps/week4/azure_faas/HttpTriggerHandler/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import datetime
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    ctime = datetime.datime.now().time()
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully at {ctime}.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
