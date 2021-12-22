52c29516c41405c60cebc10397467d3afdc6a5d6
ericdwkim <ericdwkim.io@gmail.com>
2021-12-22 17:52:59 -0500

    (wip): x86 arm64 architecture incompatibility bug

--- Python/3-DevOps/week4/azure_faas/HttpTriggerHandler/__init__.py
+++ Python/3-DevOps/week4/azure_faas/HttpTriggerHandler/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    ctime = datetime.datime.now().time()
+    ctime = datetime.dateime.now().time()
 
     name = req.params.get('name')
     if not name:
